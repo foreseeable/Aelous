a4f4052992545b6ffedcf3b2e6792cd26b6fe43d
yujie6 <yujie6@sjtu.edu.cn>
2020-01-15 17:47:22 +0800

    use rule-based mothods to extract more labels

--- label.py
+++ label.py
@@ -7,13 +7,30 @@ def is_nan(x):
     return x is np.nan or x != x
 
 
+def CountHyphen(url):
+    return url.count('-')
+
+
+def ResetProba(df, id):
+    df['media_introduction'][id] = 0.0
+    df['others'][id] = 0.0
+    df['location'][id] = 0.0
+    df['social_media_profile'][id] = 0.0
+    df['encyclopedia'][id] = 0.0
+    df['qa_forum'][id] = 0.0
+    df['shopping_item'][id] = 0.0
+    df['list'][id] = 0.0
+    df['media_player'][id] = 0.0
+    df['article'][id] = 0.0
+
+
 data = pd.read_csv('./data/dataset.csv')
 data.sum()
 category = data['categories']
 
+
 # X_train = data['url']
 # y_train = data['Categories']
-
 
 
 def find_category():
@@ -57,4 +74,70 @@ def label_engineering():
     result.to_csv('./data/dataset_v1.csv', index=False)
 
 
-label_engineering()
+def LabelByRule(sum_label=0):
+    data_v1 = pd.read_csv('./data/dataset_v1.csv')
+    category = data_v1['categories']
+    url = data_v1['url']
+    import extract_feature
+    for id in np.arange(len(data_v1)):
+        if not is_nan(category[id]):
+            sum_label += 1
+        else:
+            ResetProba(data_v1, id)
+            # print('Processing the dataline: ', id)
+            url_t = url[id]
+            is_blog = CountHyphen(url_t) > 3
+            if 'wikipeida' in url_t:
+                data_v1['NewCategory'][id] = 'encyclopedia'
+                data_v1['encyclopedia'][id] = 1.0
+                print('Find one encyclopedia: ', url_t)
+                sum_label += 1
+            elif 'stackoverflow' in url_t or 'reddit' in url_t:
+                data_v1['NewCategory'][id] = 'qa_forum'
+                data_v1['qa_forum'][id] = 1.0
+                print('Find one qa_forum: ', url_t)
+                sum_label += 1
+            elif 'profile' in url_t or 'scholar.google' in url_t:
+                data_v1['NewCategory'][id] = 'social_media_profile'
+                data_v1['social_media_profile'][id] = 1.0
+                print('Find one social_media_profile: ', url_t)
+                sum_label += 1
+
+            if 'quora' in url_t:
+                data_v1['NewCategory'][id] = 'qa_forum'
+                data_v1['qa_forum'][id] = 1.0
+                print('Find one qa_forum: ', url_t)
+                sum_label += 1
+
+            elif 'twitter' in url_t:
+                data_v1['NewCategory'][id] = 'social_media_profile'
+                data_v1['social_media_profile'][id] = 1.0
+                print('Find one social_media_profile: ', url_t)
+                sum_label += 1
+
+            elif 'imdb' in url_t:
+                if 'name' in url_t:
+                    data_v1['NewCategory'][id] = 'social_media_profile'
+                    data_v1['social_media_profile'][id] = 1.0
+                    print('Find one social_media_profile: ', url_t)
+                else:
+                    data_v1['NewCategory'][id] = 'media_introduction'
+                    data_v1['media_introduction'][id] = 1.0
+                    print('Find one media_introduction: ', url_t)
+                sum_label += 1
+
+            elif CountHyphen(url_t) > 3:
+                data_v1['NewCategory'][id] = 'article'
+                data_v1['article'][id] = 0.7
+                data_v1['qa_forum'][id] = 0.3
+                sum_label += 1
+                # print('Find one article: ', url_t)
+
+    data_v1.to_csv('./data/more_label.csv')
+    print('Labeled data in total: ', sum_label)
+
+
+if __name__ == '__main__':
+    LabelByRule()
+
+# label_engineering()
